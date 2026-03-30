ec01b78743ebe672a56854a6f8e16c3742d0aa20
Alifsvoid <alif.alfattah@gmail.com>
2026-03-30 23:08:57 +0600

    Initial commit

---
+++ src/main/java/com/spring26/section2/group13/movieproductionhouse/LoginViewController.java
@@ -0,0 +1,4 @@
+package com.spring26.section2.group13.movieproductionhouse;
+
+public class LoginViewController {
+}
---
+++ src/main/java/com/spring26/section2/group13/movieproductionhouse/helpers/BinaryFileHelper.java
@@ -0,0 +1,4 @@
+package com.spring26.section2.group13.movieproductionhouse.helpers;
+
+public class BinaryFileHelper {
+}
