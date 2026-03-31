d3953c83e9ee180931618b3cfa94b935f03c03e9
5AF1N <1921111@iub.edu.bd>
2026-04-01 00:07:03 +0600

    Initial commit

---
+++ src/main/java/com/spring26/section2/group13/movieproductionhouse/Safin/ProducerDashController.java
@@ -0,0 +1,45 @@
+package com.spring26.section2.group13.movieproductionhouse.Safin;
+
+import javafx.event.ActionEvent;
+
+public class ProducerDashController {
+    @javafx.fxml.FXML
+    public void signoutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void prodHomeOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void setLocationOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void inventoryStatusOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void emergencyReqOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dailyCallSheetOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void componentCompletionOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void secureMessagingOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logIncidentOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void directiveOA(ActionEvent actionEvent) {
+    }
+}
